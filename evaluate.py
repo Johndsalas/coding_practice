e70f126a96806c9e62d0d0a79c42a3ac4376ef92
John Salas <johndsalas@gmail.com>
2020-10-01 17:05:25 -0500

    finished tips evaluate exersize

--- evaluate.py
+++ evaluate.py
@@ -72,14 +72,18 @@ def better_then_baseline(actual,prediction):
         return True
     else:
         return False
+
 # model_significance(ols_model): 
 # that takes the ols model as input and 
 # returns the amount of variance explained in your model, and
-# the value telling you whether your model is significantly better than the baseline model (Hint: use the rsquared and 
+# the value telling you whether your model is significantly better than the baseline model 
+# (Hint: use the rsquared and 
 # f_pvalue properties from the ols model) 
 
-# Load the mpg dataset and 
-# fit a model that predicts highway mileage based on engine displacement. 
-# Take a look at all the regression evaluation metrics,
-#  and determine whether this model is better than the baseline model. 
-#  Use the functions from your evaluate.py to help accomplish this.
+def model_significance(ols_model): 
+
+    f_pval = ols_model.f_pvalue
+
+    r2 = ols_model.rsquared
+
+    return f_pval, r2
