b83bb492ca46575b8678d3030142ef20cb792314
John Salas <johndsalas@gmail.com>
2021-05-27 22:57:34 -0500

    commit

---
+++ where.sql
@@ -0,0 +1,14 @@
+
+
+-- 1.	Create a file named where_exercises.sql. Make sure to use the employees database.
+-- 2.	Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. Enter a comment with the number of records returned.
+-- 3.	Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. Enter a comment with the number of records returned. Does it match number of rows from Q2?
+-- 4.	Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. Enter a comment with the number of records returned.
+-- 5.	Find all current or previous employees whose last name starts with 'E'. Enter a comment with the number of employees whose last name starts with E.
+-- 6.	Find all current or previous employees whose last name starts or ends with 'E'. Enter a comment with the number of employees whose last name starts or ends with E. How many employees have a last name that ends with E, but does not start with E?
+-- 7.	Find all current or previous employees employees whose last name starts and ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E. How many employees' last names end with E, regardless of whether they start with E?
+-- 8.	Find all current or previous employees hired in the 90s. Enter a comment with the number of employees returned.
+-- 9.	Find all current or previous employees born on Christmas. Enter a comment with the number of employees returned.
+-- 10.	Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with the number of employees returned.
+-- 11.	Find all current or previous employees with a 'q' in their last name. Enter a comment with the number of records returned.
+-- 12.	Find all current or previous employees with a 'q' in their last name but not 'qu'. How many employees are found?
