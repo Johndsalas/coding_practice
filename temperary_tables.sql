f2f33f79a8a4e7c8fc5d117139624f9426033de8
John Salas <johndsalas@gmail.com>
2021-06-11 17:41:34 -0500

    pushing sql practice

--- temperary_tables.sql
+++ temperary_tables.sql
@@ -7,8 +7,7 @@ CREATE TEMPORARY TABLE bayes_828.employees_with_departments AS
 SELECT first_name, last_name, dept_name
 FROM employees
 JOIN dept_emp USING(emp_no)
-JOIN departments USING(dept_no)
-LIMIT 100;
+JOIN departments USING(dept_no);
 
 select * from bayes_828.employees_with_departments;
 -- a.	Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
@@ -28,6 +27,27 @@ drop column first_name;
 alter table bayes_828.employees_with_departments 
 drop column last_name;
 -- d.	What is another way you could have ended up with this same table?
+
 -- 2.	Create a temporary table based on the payment table from the sakila database.
+drop temporary table bayes_828.temp_sakila;
+
+select * from bayes_828.temp_sakila;
+
+create temporary table bayes_828.temp_sakila as
+select amount from sakila.payment;
 -- Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
+
+ALTER TABLE bayes_828.temp_sakila
+ALTER COLUMN amount INT;
+
+update bayes_828.temp_sakila  -- Out of range value for column 'amount' at row 44
+set amount = amount * 100;
+
+create temporary table bayes_828.temp_sakila2 as
+select amount * 100 from sakila.payment;
+
+ALTER TABLE bayes_828.temp_sakila2
+ALTER COLUMN amount INT;
+
+select * from bayes_828.temp_sakila2;
 -- 3.	Find out how the current average pay in each department compares to the overall, historical average pay. In order to make the comparison easier, you should use the Z-score for salaries. In terms of salary, what is the best department right now to work for? The worst?
